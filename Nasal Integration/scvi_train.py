f083ac74aa7d9800c3d357ab19ee2aa281302f43
Connor Puritz <connorpuritz2025@u.northwestern.edu>
2023-08-03 14:40:52 -0500

    Integration

---
+++ Nasal Integration/scvi_train.py
@@ -0,0 +1,5 @@
+import scvi
+data_dir = "/projects/b1038/Pulmonary/cpuritz/PASC/data/NEP_exon_only/models"
+model_adata = scvi.model.SCVI.load(data_dir)
+model_adata.train(max_epochs = 500, use_gpu = True, check_val_every_n_epoch = 2, early_stopping = True)
+model_adata.save(data_dir, overwrite = True, prefix = "trained")
