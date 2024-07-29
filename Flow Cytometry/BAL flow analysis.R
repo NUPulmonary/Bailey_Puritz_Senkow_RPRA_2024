4c2a8da4bfa0860d4d4bee3f72544311b3063730
Connor Puritz <connorpuritz2025@u.northwestern.edu>
2024-07-29 12:10:52 -0500

    Updated flow cytometry analysis

--- Flow Cytometry/BAL flow analysis.R
+++ Flow Cytometry/BAL flow analysis.R
@@ -1,9 +1,7 @@
-setwd("/projects/b1038/Pulmonary/cpuritz/PASC/data")
 set.seed(1, kind = "L'Ecuyer-CMRG")
 
 library(ggplot2)
 library(dplyr)
-library(tidyr)
 library(tibble)
 library(patchwork)
 library(coin)
@@ -14,42 +12,52 @@ library(reshape2)
 library(ComplexHeatmap)
 library(RColorBrewer)
 
-source("../code/util/pairwise_wilcox_test.R")
+source("code/util/pairwise_wilcox_test.R")
 
-## Read in data
-flow <- read.csv("deidentified_data/deidentified_flow_cytometry_data.csv",
-                 check.names = F)
+# Read in data
+flow <- read.csv("data/deidentified_data/20230407_PASC_flow_V4_deidentified.csv",
+                 check.names = FALSE)
 flow <- na.omit(flow)
 rownames(flow) <- NULL
-colnames(flow) <- gsub("macrophages", "MPs", colnames(flow))
+flow <- flow %>%
+    dplyr::rename("Dendritic cells" = "C4 HLADR_high",
+                  C1_FSC_high = "C1 FSC_high",
+                  C3_HLADR_low = "C3 HLADR_low") %>%
+    dplyr::mutate(Other = C1_FSC_high + C3_HLADR_low) %>%
+    dplyr::select(-c("Eosinophils", "C2 FSC_low", "C5 SSC_low", "C6 SSC_low",
+                     "C1_FSC_high", "C3_HLADR_low")) %>%
+    dplyr::rename("CD206 high MPs" = "CD206 high macrophages",
+                  "CD206 low MPs" = "CD206 low macrophages")
 
 # Group separates RPRA from RPRA (transplant), Status does not
 flow["Status"] <- gsub(" (transplant)", "", flow$Group, fixed = TRUE)
 
-## Renormalize abundances (rounding errors)
 data_cols <- which(!colnames(flow) %in% c("Study_ID", "Group", "Status"))
-flow[data_cols] <- t(apply(flow[data_cols], 1, function(x) { x / sum(x) * 100 }))
 cell_types <- colnames(flow[data_cols])
 
-## Perform pairwise Wilcoxon rank sum tests with FDR correction
-all_comps <- boxplot_signif(flow, cell_types, group.var = "Status",
-                            var.name = "cell_type", annot = "value")
+# Perform pairwise Wilcoxon rank sum tests with FDR correction
+all_comps <- boxplot_signif(df = flow,
+                            cols = cell_types,
+                            group.var = "Status",
+                            var.name = "cell_type",
+                            annot = "value")
 
-## Create boxplots
+# Create boxplots
 palette <- setNames(pal_npg("nrc")(3), c("RPRA", "Healthy", "RPRA (transplant)"))
 signif_plots <- list()
 nonsignif_plots <- list()
 for (i in 1:length(cell_types)) {
+    cell <- cell_types[i]
     data <- flow %>%
-        dplyr::select(dplyr::all_of(c("Status", cell_types[i]))) %>%
-        dplyr::rename(cell_type = cell_types[i])
-    comparisons <- dplyr::filter(all_comps, cell_type == cell_types[i])
+        dplyr::select(dplyr::all_of(c("Status", cell))) %>%
+        dplyr::rename(cell_type = cell)
+    comparisons <- dplyr::filter(all_comps, cell_type == cell)
     pplt <- ggplot(data, aes(x = Status, y = cell_type, fill = Status)) +
         geom_boxplot(outlier.shape = NA) +
         geom_jitter(shape = 16, position = position_jitter(width = 0.25)) +
         xlab(NULL) +
         ylab(NULL) +
-        ggtitle(gsub("MPs", "macrophages", cell_types[i])) +
+        ggtitle(cell) +
         scale_fill_manual(values = palette[1:2]) +
         theme_bw(base_family = "Arial") +
         theme(text = element_text(family = "Arial"),
@@ -63,7 +71,8 @@ for (i in 1:length(cell_types)) {
               panel.grid.minor.y = element_blank())
     
     if (dim(comparisons)[1] > 0) {
-        signif_plots[[length(signif_plots) + 1]] <- pplt +
+        # Add significance bar
+        pplt <- pplt +
             ylim(c(0, 1.06 * max(comparisons$y))) +
             geom_signif(xmin = comparisons$xmin,
                         xmax = comparisons$xmax,
@@ -72,23 +81,35 @@ for (i in 1:length(cell_types)) {
                         tip_length = 0,
                         vjust = -0.225,
                         textsize = 3.5)
+        # Move Other to supplement
+        if (cell != "Other") {
+            signif_plots <- c(signif_plots, list(pplt))
+        } else {
+            nonsignif_plots <- c(nonsignif_plots, list(pplt))
+        }
     } else {
-        nonsignif_plots[[length(nonsignif_plots) + 1]] <- pplt
+        nonsignif_plots <- c(nonsignif_plots, list(pplt))
     }
 }
 
 ylabel <- ggplot(data.frame(l = "Percent of total cells", x = 1, y = 1)) +
     geom_text(aes(x, y, label = l), angle = 90, size = 4) +
-    theme_void() + coord_cartesian(clip = "off")
-signif_bps <- ylabel + wrap_plots(signif_plots, nrow = 1) +
+    theme_void() +
+    coord_cartesian(clip = "off")
+signif_bps <- ylabel +
+    wrap_plots(signif_plots, nrow = 1) +
     plot_layout(widths = c(1, 30))
-nonsignif_bps <- ylabel + wrap_plots(nonsignif_plots, ncol = 2) +
+nonsignif_bps <- ylabel +
+    wrap_plots(nonsignif_plots, ncol = 2) +
     plot_layout(widths = c(1, 30))
 
 
-## Hierarchical clustering
+# Hierarchical clustering
 flow2 <- column_to_rownames(flow, "Study_ID")
 group_annot <- dplyr::select(flow2, Diagnosis = Group)
+group_annot$Cluster <- c(3, 1, 3, 1, 1, 1, 1, 2, 3, 4, 1, 2, 3,
+                         3, 2, 3, 1, 3, 3, 4, 1, 4, 4, 4, 3, 3,
+                         2, 2, 2, 2, 5, 5, 2, 2, 5, 3, 5, 5, 2)
 flow_mat <- t(as.matrix(dplyr::select(flow2, -Group, -Status)))
 
 pheatmap2ggplot <- function (ph) {
@@ -97,13 +118,14 @@ pheatmap2ggplot <- function (ph) {
 ph_pal <- colorRampPalette(rev(brewer.pal(n = 7, name = "RdBu")))(1001)
 
 scale_rows <- function(x) {
-    m = apply(x, 1, mean, na.rm = T)
-    s = apply(x, 1, sd, na.rm = T)
+    m <- apply(x, 1, mean)
+    s <- apply(x, 1, sd)
     return((x - m) / s)
 }
 fmat_scaled <- scale_rows(flow_mat)
 
-s <- ceiling(max(abs(min(fmat_scaled)), max(fmat_scaled)))
+s <- ceiling(max(abs(min(fmat_scaled)), abs(max(fmat_scaled))))
+cl_palette <- setNames(pal_npg("nrc")(8)[4:8], as.character(seq(5)))
 heatmap <- ComplexHeatmap::pheatmap(
     fmat_scaled,
     cluster_rows = T,
@@ -118,17 +140,17 @@ heatmap <- ComplexHeatmap::pheatmap(
     color = ph_pal,
     breaks = seq(-s, s, length.out = length(ph_pal)),
     annotation_col = group_annot,
-    annotation_color = list(Diagnosis = palette),
-    name = "Percent",
+    annotation_color = list(Diagnosis = palette, Cluster = cl_palette),
+    name = "Row\nz-score",
     heatmap_legend_param = list(legend_height = unit(5, "cm"),
                                 at = seq(-s, s, s/2))
 )
 heatmap <- pheatmap2ggplot(heatmap)
 
 
-## Correlation with CT features
+# Correlation with CT features
 ct_features <- c("Normal", "Fibrotic", "Inflammatory", "Nodularity")
-ct_data <- read.csv("deidentified_data/deidentified_ct_scan_1_data.csv",
+ct_data <- read.csv("data/deidentified_data/deidentified_ct_scan_1_data.csv",
                     check.names = FALSE)
 ct_data <- ct_data %>% dplyr::rename(Study_ID = Subject_ID)
 flow_ct <- flow %>%
@@ -141,21 +163,23 @@ cor_mat <- matrix(nrow = length(cell_types),
 p_mat <- matrix(nrow = length(cell_types),
                 ncol = length(ct_features),
                 dimnames = list(cell_types, ct_features))
+cdst <- coin::approximate(nresample = 1e6,
+                          parallel = "multicore",
+                          ncpus = 16)
 for (i in 1:length(cell_types)) {
     for (j in 1:length(ct_features)) {
-        x <- flow_ct[,cell_types[i]]
-        y <- flow_ct[,ct_features[j]]
+        x <- flow_ct[, cell_types[i]]
+        y <- flow_ct[, ct_features[j]]
         cor_mat[i, j] <- cor(x, y, method = "spearman")
-        test <- coin::spearman_test(x ~ y, data = data.frame(x, y),
-                                    distribution = coin::approximate(nresample = 1e6,
-                                                                     parallel = "multicore",
-                                                                     ncpus = 16))
-        p_mat[i, j] <- coin::pvalue(test)[1]
+        test <- coin::spearman_test(x ~ y,
+                                    data = data.frame(x, y),
+                                    distribution = cdst)
+        p_mat[i, j] <- as.numeric(coin::pvalue(test))
     }
 }
 
-cor_df <- cor_mat %>% melt() %>% dplyr::rename(Correlation = value)
-p_df <- p_mat %>% melt() %>% dplyr::rename(pval = value)
+cor_df <- dplyr::rename(melt(cor_mat), Correlation = value)
+p_df <-  dplyr::rename(melt(p_mat), pval = value)
 p_adj <- p.adjust(as.vector(p_df$pval, mode = "numeric"), method = "fdr")
 
 # Add significance codes
@@ -171,8 +195,8 @@ ct_corr <- ComplexHeatmap::pheatmap(
     cor_mat,
     cluster_rows = T,
     cluster_cols = T,
-    cellwidth = 20,
-    cellheight = 20,
+    cellwidth = 21,
+    cellheight = 21,
     angle_col = '45',
     clustering_method = "ward.D2",
     scale = "none",
@@ -187,7 +211,7 @@ ct_corr <- ComplexHeatmap::pheatmap(
 )
 ct_corr <- pheatmap2ggplot(ct_corr)
 
-## Point and line plots
+# Point and line plots
 plplots <- list()
 for (i in 1:length(cell_types)) {
     for (j in 1:length(ct_features)) {
@@ -195,15 +219,20 @@ for (i in 1:length(cell_types)) {
         ct <- ct_features[j]
         fname <- gsub('-|/| ', '_', paste(cell, ct, sep = '_'))
         
-        df <- data.frame(x = flow_ct[,ct], y = flow_ct[,cell], Group = flow_ct$Group)
+        df <- data.frame(x = flow_ct[, ct],
+                         y = flow_ct[, cell],
+                         Group = flow_ct$Group)
         cr <- deparse(sprintf("%.3f", cor(df$x, df$y, method = "s")))
         pplt <- ggplot(df, aes(x, y)) +
             geom_point() +
             stat_smooth(method = "lm", formula = y ~ x) +
             xlab(paste(ct, "fraction of scan")) +
             ylab(NULL) +
-            ggtitle(gsub("MPs", "macrophages", cell)) +
-            annotate("text", x = 0.73 * max(df$x), y = 0.98 * max(df$y), hjust = 0,
+            ggtitle(cell) +
+            annotate("text",
+                     x = 0.74 * max(df$x),
+                     y = 0.98 * max(df$y),
+                     hjust = 0,
                      label = paste0("rho == ", cr),
                      parse = TRUE) +
             theme_bw() +
@@ -220,41 +249,16 @@ for (i in 1:length(cell_types)) {
     }
 }
 
-### Build figures
+## Build figures
 get_annot <- function (x) {
-    plot_annotation(title = x, theme = theme(plot.title = element_text(face = 2, size = 20)))
+    plot_annotation(
+        title = x,
+        theme = theme(plot.title = element_text(face = 2, size = 20))
+    )
 }
 
-## Build Figure 2
-cl_bar <- ggplot(data = data.frame(
-        x = c(-0.05, -0.05, 2.57, 4.88, 7.2, 10),
-        xend = c(10, -0.05, 2.57, 4.88, 7.2, 10),
-        y = 0.1,
-        yend = c(0.1, 0.14, 0.14, 0.14, 0.14, 0.14))
-    ) +
-    geom_segment(aes(x = x, xend = xend, y = y, yend = yend)) +
-    annotate("text", x = 1.3, y = 0.07, label = "Cluster 1") +
-    annotate("text", x = 3.8, y = 0.07, label = "Cluster 2") +
-    annotate("text", x = 6.0, y = 0.07, label = "Cluster 3") +
-    annotate("text", x = 8.6, y = 0.07, label = "Cluster 4") +
-    xlab(NULL) +
-    ylab(NULL) +
-    ylim(c(0, 1)) +
-    theme_minimal() +
-    theme(axis.line = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.text.x = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.ticks.y = element_blank())
-fig_2a <- wrap_elements(
-    heatmap +
-    inset_element(cl_bar, 0.025, -0.16, 0.809, 0.9) +
-    get_annot("a")
-)
+# Build Figure 2
+fig_2a <- wrap_elements(heatmap + get_annot("a"))
 fig_2b <- wrap_elements(signif_bps + get_annot("b"))
 fig_2c <- wrap_elements(ct_corr + get_annot("c"))
 
@@ -266,14 +270,18 @@ fig2 <- fig_2a + fig_2b + fig_2c +
         heights = c(1, 0.7),
         widths = c(0.61, 0.39)
     )
-ggsave(plot = fig2, filename = "figures/fig_2/fig2.pdf", width = 16, height = 9.5,
-       device = cairo_pdf)
+ggsave(plot = fig2,
+       filename = "data/figures/fig_2/fig2.pdf",
+       width = 16, height = 10.5, device = cairo_pdf)
 
 
-## Build Figure S2
-fnames <- c("Neutrophils_Normal", "Neutrophils_Inflammatory",
-            "Neutrophils_Nodularity", "CD4_T_cells_Fibrotic",
-            "B_and_plasma_cells_Fibrotic", "CD206_high_MPs_Nodularity")
+# Build Figure S2
+fnames <- c("Neutrophils_Normal",
+            "Neutrophils_Inflammatory",
+            "Neutrophils_Nodularity",
+            "CD4_T_cells_Fibrotic",
+            "B_and_plasma_cells_Fibrotic",
+            "CD206_high_MPs_Nodularity")
 ylabel <- ggplot(data.frame(l = "Percent of total cells", x = 1, y = 1)) +
     geom_text(aes(x, y, label = l), angle = 90, size = 4) +
     theme_void() +
@@ -286,5 +294,6 @@ fig_s2a <- wrap_elements(nonsignif_bps + get_annot("a"))
 fig_s2b <- wrap_elements(ct_plots + get_annot("b"))
 fig_s2 <- fig_s2a + fig_s2b + plot_layout(ncol = 2)
 
-ggsave(plot = fig_s2, filename = "figures/fig_s2/fig_s2.pdf",
+ggsave(plot = fig_s2,
+       filename = "data/figures/fig_s2/fig_s2.pdf",
        width = 14, height = 10, device = cairo_pdf)
